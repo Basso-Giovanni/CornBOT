7e223ad5fa8675d2563336884bb51429c89210ad
Basso-Giovanni <9698115@itisrossi.vi.it>
2024-12-12 19:08:58 +0100

    Aggiunta del bottone Biografia sotto le info della persona

--- src/CornBOT.java
+++ src/CornBOT.java
@@ -1,10 +1,16 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 /** Classe per la gestione del bot
  *
@@ -57,6 +63,30 @@ public class CornBOT extends TelegramLongPollingBot
             else
                 sendMessage(chatId, "Comando non riconosciuto 🤔. Usa /help per vedere i comandi disponbili.");
         }
+        if (update.hasCallbackQuery())
+        {
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+            if (update.getCallbackQuery().getData().contains("biografia_"))
+            {
+                try
+                {
+                    String nome = update.getCallbackQuery().getData().split("_")[1];
+                    String query = "SELECT biografia FROM Soggetto WHERE nome LIKE ?";
+                    ResultSet rs = DB_Manager.query(query, "%" + nome + "%");
+
+                    if (rs.next())
+                    {
+                        String reply = "BIOGRAFIA DI " + nome.toUpperCase(Locale.ROOT) + " 📖\n" +
+                            rs.getString("biografia");
+                        sendMessage(chatId, reply);
+                    }
+                }
+                catch (SQLException e)
+                {
+                    System.out.println("⚠️ Errore nella richiesta della biografia di " + update.getCallbackQuery().getData().split("_")[1]);
+                }
+            }
+        }
     }
 
     private void cercaFilm(Long chatId, String titolo)
@@ -92,15 +122,27 @@ public class CornBOT extends TelegramLongPollingBot
         String sql = "SELECT * FROM Soggetto WHERE nome LIKE ?";
         try
         {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
             ResultSet rs = DB_Manager.query(sql, "%" + nome + "%");
             if (rs.next())
             {
-                String reply = "Nome  👨‍🎤️: " + rs.getString("titolo") +
-                        "\nData di nascita 🎂: " + rs.getInt("anno_produzione") +
-                        "\nLuogo di nascita 🗺️: " + rs.getString("genere");
+                String reply = "Nome  🎭️: " + rs.getString("nome") +
+                        "\nData di nascita 🎂: " + rs.getDate("data_nascita").toLocalDate().format(formatter) +
+                        "\nLuogo di nascita 🗺️: " + rs.getString("luogo_nascita");
                 if (rs.getDate("data_morte") != null )
-                    reply += "\nData di morte ⚰️: " + rs.getDate("data_morte").toLocalDate();
-                sendMessage(chatId, reply);
+                    reply += "\nData di morte ⚰️: " + rs.getDate("data_morte").toLocalDate().format(formatter);
+
+                InlineKeyboardMarkup ikm = new InlineKeyboardMarkup();
+
+                List<InlineKeyboardButton> lista_btn = new ArrayList<>();
+                InlineKeyboardButton btn = new InlineKeyboardButton();
+                    btn.setText("Biografia");
+                    btn.setCallbackData("biografia_" + rs.getString("nome"));
+                lista_btn.add(btn);
+                List<List<InlineKeyboardButton>> riga = new ArrayList<>();
+                riga.add(lista_btn);
+                ikm.setKeyboard(riga);
+                sendMessage(chatId, reply, ikm);
             }
             else
                 sendMessage(chatId, "Persona non trovata 😣");
@@ -108,6 +150,22 @@ public class CornBOT extends TelegramLongPollingBot
         catch (SQLException e)
         {
             sendMessage(chatId, "⚠️ Errore nella ricerca della persona: " + nome);
+        }
+    }
+
+    private void sendMessage(Long chatId, String text, InlineKeyboardMarkup ikm)
+    {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId.toString());
+        message.setText(text);
+        message.setReplyMarkup(ikm);
+        try
+        {
+            execute(message);
+        }
+        catch (TelegramApiException e)
+        {
+            System.out.println("⚠️ Errore nell'invio del messaggio!");
         }
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main
 {
     public static void main(String[] args)
     {
-        TMDB_Scraper.Film_scraper("https://www.themoviedb.org/movie/106646-the-wolf-of-wall-street", 106646);
-//        try
-//        {
-//            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            botsApi.registerBot(new CornBOT());
-//            System.out.println("Bot avviato!");
-//        }
-//        catch (Exception e)
-//        {
-//            e.printStackTrace();
-//        }
+//        TMDB_Scraper.Film_scraper("https://www.themoviedb.org/movie/106646-the-wolf-of-wall-street", 106646);
+        try
+        {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new CornBOT());
+            System.out.println("Bot avviato!");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
